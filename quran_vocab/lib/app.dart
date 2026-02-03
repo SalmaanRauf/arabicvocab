3ed45a165ac42ec8cb506f1cf9143f7e0a758b75
SalmaanRauf <raufsalmaan@csu.fullerton.edu>
2026-02-03 13:13:26 -0800

    feat: add global theme mode

--- quran_vocab/lib/app.dart
+++ quran_vocab/lib/app.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
 import 'presentation/routes/app_router.dart';
+import 'presentation/state/settings_providers.dart';
 import 'presentation/theme/app_theme.dart';
 
-class QuranVocabApp extends StatelessWidget {
+class QuranVocabApp extends ConsumerWidget {
   const QuranVocabApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final GoRouter router = AppRouter.router;
+    final themeMode = ref.watch(themeModeProvider);
     return MaterialApp.router(
       title: 'Quranic Vocabulary',
       theme: AppTheme.light(),
+      darkTheme: AppTheme.dark(),
+      themeMode: themeMode,
       routerConfig: router,
       debugShowCheckedModeBanner: false,
     );
